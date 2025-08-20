bf59fdbae37f3f491b532faa70a62ee4d58d0d99
ShubhamKumar547 <kumarshubh054@gmail.com>
2025-08-21 02:16:24 +0530

    Added Navbar component

--- Club_Website/tailwind.config.js
+++ Club_Website/tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+
+    fontSize: {
+      
+      basefnt: "1.5rem",
+      headfnt:"1.7rem"
+    },
   },
   plugins: [],
-}
+};
