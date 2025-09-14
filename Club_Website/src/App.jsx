142001db764218d88f288d76ae15d67f39cc8d24
hitkavyam <hitkavyam1434@gmail.com>
2025-09-15 01:06:25 +0530

    added home-verticals component

--- Club_Website/src/App.jsx
+++ Club_Website/src/App.jsx
@@ -1,12 +1,17 @@
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import HomeVerticals from "./components/HomeVerticals";
+
 
 function App() {
   return (
     <div>
       <h1 className="text-3xl font-bold ">
         <Navbar />
+        <HomeVerticals />
+
       </h1>
+
 
       <div>
         <Home/>
