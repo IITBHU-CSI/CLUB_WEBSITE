849df64912a250e4e3b772cb8870af6f23a1aa13
Aryan8317 <aryangautam8317@gmail.com>
2025-10-13 15:38:39 +0530

    Fix errors

--- Club_Website/src/App.jsx
+++ Club_Website/src/App.jsx
@@ -6,9 +6,9 @@ import Footer from "./components/Footer";
 import Front from "./components/Front";
 import HomeVerticals from "./components/HomeVerticals";
 import Events from "./components/Events";
-import Gallery from "./Pages/Gallery";
-import HomePage from "./Pages/HomePage";
-import OurTeam from "./Pages/OurTeam";
+import Gallery from "./pages/Gallery";
+import HomePage from "./pages/HomePage";
+import OurTeam from "./pages/OurTeam";
 
 function App() {
   return (
